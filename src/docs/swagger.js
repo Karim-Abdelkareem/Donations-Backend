57177b1d84c8c1dfc72839ea822f3e6fbd718a2a
Karim Abdelkareem <alkar33m@gmail.com>
2025-05-03 02:00:48 +0300

    swagger test

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,28 +1,3 @@
-// import swaggerJSDoc from "swagger-jsdoc";
-
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Donation API",
-//       version: "1.0.0",
-//       description: "توثيق API الخاص بمشروع التبرع",
-//     },
-//     servers: [
-//       {
-//         url: "https://your-vercel-domain.vercel.app/api", // عدل الرابط بعد النشر
-//       },
-//       {
-//         url: "http://localhost:5001/api", // للسيرفر المحلي أثناء التطوير
-//       },
-//     ],
-//   },
-//   apis: ["./src/modules/**/*.js"], // مسارات ملفات الراوتات
-// };
-
-// const swaggerSpec = swaggerJSDoc(options);
-
-// export default swaggerSpec;
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
@@ -30,22 +5,20 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Your API Title",
+      title: "Your API",
       version: "1.0.0",
-      description: "API documentation",
     },
     servers: [
       {
-        url: "https://your-vercel-domain.vercel.app/api", // عدل الرابط بعد النشر
+        url: "https://donations-backend-ten.vercel.app",
       },
       {
-        url: "http://localhost:3000", // replace with Vercel URL in prod
+        url: "http://localhost:3000",
       },
     ],
   },
-  apis: ["./routes/*.js"], // adjust path if needed
+  apis: ["./routes/*.js"],
 };
 
 const specs = swaggerJsdoc(options);
-
 export { swaggerUi, specs };
